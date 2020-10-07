180ca53581e55a2f9d659c0bdb25bea581f6297b
AmRo <virtualemailbox045@gmail.com>
2020-10-07 17:17:41 +0330

    Create fake data for ItemsControl

--- Source/WpfCustomScrollBar.Demo/MainWindow.xaml.cs
+++ Source/WpfCustomScrollBar.Demo/MainWindow.xaml.cs
@@ -1,10 +1,36 @@
-﻿namespace WpfCustomScrollBar.Demo
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace WpfCustomScrollBar.Demo
 {
     public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+            AddDummyItems();
+            DataContext = this;
+        }
+
+        public List<string> DummyItems { get; set; }
+
+        private void AddDummyItems()
+        {
+            DummyItems = new List<string>();            
+
+            for (var i = 0; i < 100; ++i)
+            {
+                var itemBuilder = new StringBuilder();
+
+                for (var j = 0; j <= i; j++)
+                {
+                    itemBuilder
+                        .Append((j + 1).ToString().PadLeft(3, '0'))
+                        .Append("  ");
+                }
+
+                DummyItems.Add(itemBuilder.ToString());
+            }
         }
     }
 }
