239bbad4446a6d39cf65c2c18fe0d851b7605d4a
AmRo <virtualemailbox045@gmail.com>
2020-10-09 15:49:27 +0330

    Remove custom control and use attached properties instead

---
+++ Source/WpfCustomScrollBar/Converters/ScrollBarSizeToMarginConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WpfCustomScrollBar.Converters
+{
+    public class ScrollBarSizeToMarginConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var size = System.Convert.ToDouble(value, culture);
+            var isVertical = System.Convert.ToBoolean(parameter, culture);
+
+            return isVertical ? new Thickness(-size, 0, 0, 0) : new Thickness(0, -size, size, 0);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => Binding.DoNothing;
+    }
+}
---
+++ Source/WpfCustomScrollBar/ScrollBarAssist.cs
@@ -0,0 +1,67 @@
+﻿using System.Windows;
+
+namespace WpfCustomScrollBar
+{
+    public static class ScrollBarAssist
+    {
+        #region AttachedProperty : Size
+
+        public static double GetSize(DependencyObject obj)
+            => (double)obj.GetValue(SizeProperty);
+
+        public static void SetSize(DependencyObject obj, double value)
+            => obj.SetValue(SizeProperty, value);
+
+        public static readonly DependencyProperty SizeProperty =
+            DependencyProperty.RegisterAttached("Size",
+                typeof(double), typeof(ScrollBarAssist),
+                new UIPropertyMetadata(default));
+
+        #endregion
+
+        #region AttachedProperty : ButtonsVisibility
+
+        public static Visibility GetButtonsVisibility(DependencyObject obj)
+            => (Visibility)obj.GetValue(ButtonsVisibilityProperty);
+
+        public static void SetButtonsVisibility(DependencyObject obj, Visibility value)
+            => obj.SetValue(ButtonsVisibilityProperty, value);
+
+        public static readonly DependencyProperty ButtonsVisibilityProperty =
+            DependencyProperty.RegisterAttached("ButtonsVisibility",
+                typeof(Visibility), typeof(ScrollBarAssist),
+                new UIPropertyMetadata(default));
+
+        #endregion
+
+        #region AttachedProperty : CornerRadius
+
+        public static CornerRadius GetCornerRadius(DependencyObject obj)
+            => (CornerRadius)obj.GetValue(CornerRadiusProperty);
+
+        public static void SetCornerRadius(DependencyObject obj, CornerRadius value)
+            => obj.SetValue(CornerRadiusProperty, value);
+
+        public static readonly DependencyProperty CornerRadiusProperty =
+            DependencyProperty.RegisterAttached("CornerRadius",
+                typeof(CornerRadius), typeof(ScrollBarAssist),
+                new UIPropertyMetadata(default));
+
+        #endregion
+
+        #region AttachedProperty : ThumbCornerRadius
+
+        public static CornerRadius GetThumbCornerRadius(DependencyObject obj)
+           => (CornerRadius)obj.GetValue(ThumbCornerRadiusProperty);
+
+        public static void SetThumbCornerRadius(DependencyObject obj, CornerRadius value)
+            => obj.SetValue(ThumbCornerRadiusProperty, value);
+
+        public static readonly DependencyProperty ThumbCornerRadiusProperty =
+            DependencyProperty.RegisterAttached("ThumbCornerRadius",
+                typeof(CornerRadius), typeof(ScrollBarAssist),
+                new UIPropertyMetadata(default));
+
+        #endregion
+    }
+}
